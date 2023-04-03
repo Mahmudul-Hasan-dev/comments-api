cf5896be8fa50e70910faf38a9763c9aa7ec0623
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-04-03 16:16:55 +0600

    design card system done

--- js/app.js
+++ js/app.js
@@ -14,9 +14,17 @@ function displayComments(comments) {
     for (const comment of comments) {
         console.log(comment)
         const newComment = document.createElement('div');
-        newComment.innerHTML = `<h4>name-${comment.name}</h4>
-        <h5>email:${comment.email}</h5>
-        <h4>comment-${comment.body}</h4>`
+        // newComment.innerHTML = `<h4>name-${comment.name}</h4>
+        // <h5>email:${comment.email}</h5>
+        // <h4>comment-${comment.body}</h4>`
+        newComment.classList = "w-100 m-auto align-item-center"
+        newComment.innerHTML = `<div class="card border-success mb-3" style="max-width: 18rem;">
+        <div class="card-header bg-transparent border-success">name-${comment.name}</div>
+        <div class="card-body text-success">
+            <p class="card-text">${comment.body}</p>
+        </div>
+        <div class="card-footer bg-transparent border-success">mail-${comment.email}</div>
+         </div>`
         commentContainer.appendChild(newComment)
     }
 }
